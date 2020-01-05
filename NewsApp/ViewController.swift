dbd796f3b97d1cf10ac6ff1308c2b9c0ac732095
Aditya Jha <adityajha199@gmail.com>
2020-01-05 16:18:51 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Adi on 1/5/20.
+//  Copyright © 2020 Aditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
